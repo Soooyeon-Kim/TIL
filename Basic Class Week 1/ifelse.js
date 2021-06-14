3a6ce709dc6afec01ddff88f80233d8c89d56f38
Soooyeon-Kim <83583349+Soooyeon-Kim@users.noreply.github.com>
2021-06-14 21:17:00 +0900

    Add files via upload

---
+++ Basic Class Week 1/ifelse.js
@@ -0,0 +1,22 @@
+let age=24
+if (age > 20) {
+    console.log('성인입니다.')
+} else {
+    console.log('청소년입니다.')
+}
+// 성인입니다.
+let gender = 'male'
+if (age > 20 && gender == 'male') {
+    console.log('성인 남성 입니다.')
+} else {
+    console.log('청소년입니다.')
+}
+// 성인 남성 입니다. 
+if (age > 20) {
+    console.log('성인입니다.')
+} else if (age > 7) {
+    console.log('청소년입니다.')
+} else {
+    console.log('아동입니다.')
+}
+// 성인입니다.
