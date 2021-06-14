35a05f95c66f019570807e3d3d1e66d2950496e4
Soooyeon-Kim <83583349+Soooyeon-Kim@users.noreply.github.com>
2021-06-14 20:18:34 +0900

    Add files via upload

---
+++ Basic Class Week 1/function.js
@@ -0,0 +1,18 @@
+function sum(num1, num2) {
+	return num1+num2
+}
+
+function mysum(num1, num2) {
+	alert('Hello!')
+	return num1+num2
+}
+
+let result2 = mysum(2,3)
+
+result2
+
+function mysum(num1, num2) {
+	alert('First :'+ num1)
+	alert('Second :'+ num2)
+}
+mysum(2,3)
