04012422f05412fd1dedd95e5a24515c6119bd99
Soooyeon-Kim <83583349+Soooyeon-Kim@users.noreply.github.com>
2021-06-14 20:25:34 +0900

    Add files via upload

---
+++ Basic Class Week 1/list.js
@@ -0,0 +1,23 @@
+let a = 2
+a + 3
+// 5
+a = 5 
+a + 3
+// 8
+let firstname = "Sooyeon"
+firstname
+// "Sooyeon"
+let lastname = "Kim"
+firstname + lastname
+// SooyeonKim
+// -------- *List* --------
+let a_list = ['strawberry', 'peach', 'apple']
+a_list
+// (3) ["strawberry", "peach", "apple"]
+a_list[0]
+//"strawberry"
+//list append -> .push('element')
+a_list.push('watermelon')
+//4
+a_list
+//(4) ["strawberry", "peach", "apple", "watermelon"]
