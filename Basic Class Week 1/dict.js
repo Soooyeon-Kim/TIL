0a19c5f79381acbaf922c4571a07875b60f666bd
Soooyeon-Kim <83583349+Soooyeon-Kim@users.noreply.github.com>
2021-06-14 20:32:47 +0900

    Add files via upload

---
+++ Basic Class Week 1/dict.js
@@ -0,0 +1,20 @@
+//dict
+let a_dict = {'name': 'sooyeon', 'number' : 412}
+a_dict['name']
+// "sooyeon"
+a_dict['number']
+// 412
+//dict append 
+a_dict['height'] = 160
+//160
+a_dict 
+// {name: "sooyeon", number: 412, height: 160}
+// list in dict
+a_dict['fruits'] = a_list
+//(4) ["strawberry", "peach", "apple","watermelon"]
+a_dict
+//{name: "sooyeon", number: 412, height: 160, fruits: Array(4)}
+a_dict['fruits']
+//(4) ["strawberry", "peach", "apple","watermelon"]
+a_dict['fruits'][0]
+"strawberry"
