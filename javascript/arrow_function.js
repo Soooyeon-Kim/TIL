d963c1ce1c44aaf538b05e96041d0b2ec53fa3ea
Soooyeon-Kim <83583349+Soooyeon-Kim@users.noreply.github.com>
2021-07-27 18:04:18 +0900

    Add files via upload

---
+++ javascript/arrow_function.js
@@ -0,0 +1,25 @@
+// function
+function a(x, y) {
+   return x + y;
+}
+console.log(a(1, 2));
+
+
+// arrow function
+const a = (x, y) => {
+  return x + y ;
+}
+
+console.log(a(1,2))
+
+//
+const c = (x, y) => {
+	console.log(x, y);
+};
+c(5, 6);
+
+//
+const d = (x, y) => console.log(x, y);
+d(7, 8);
+
+
