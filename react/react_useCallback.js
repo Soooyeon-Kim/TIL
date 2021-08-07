53234a540888be72e2847f7460d15a1d4d516d16
Soooyeon-Kim <83583349+Soooyeon-Kim@users.noreply.github.com>
2021-08-07 12:31:46 +0900

    Add files via upload

---
+++ react/react_useCallback.js
@@ -0,0 +1,25 @@
+import React, {useState, useCallback} from 'react';
+
+const App = () => {
+  const [foo, setFoo] = useState(0);
+  const [bar, setBar] = useState(0);
+
+  
+  const calc = useCallback(() => {
+    return foo + bar
+  }, [foo, bar])
+
+  return (
+    <div className='App'>
+    <input value={foo} onChange={(event)=> {
+        setFoo(parseInt(event.target.value));
+    }}/>
+    <input value={bar} onChange={(event)=> {
+        setBar(parseInt(event.target.value));
+    }}/>
+    <div>{calc()}</div>
+    </div>
+
+  )
+}
+export default App;
